e0b965b875b765f4a1366157649c8bd3203dbdc0
ODawah <omar.adel@factmata.com>
2023-02-13 00:14:59 +0200

    added update user name operation

--- data_operations/ops.py
+++ data_operations/ops.py
@@ -1,7 +1,7 @@
 import uuid
 from database.sql_connector import connect
 from utilities.hasher import encrypt
-from utilities.validate import is_valid_email,is_valid_user_name
+from utilities.validate import is_valid_email, is_valid_user_name, is_valid_uuid
 
 
 # create_user: function takes user's class and add user to the database then return bool of success
@@ -23,3 +23,19 @@ def create_user(user):
     if command_handler.rowcount != 1:
         return None
     return user
+
+
+def update_user_name(name, user_uuid) -> bool:
+    db = connect()
+    if db is None:
+        return None
+    if not is_valid_user_name(name) or not is_valid_uuid(user_uuid):
+        return False
+    command_handler = db.cursor()
+    command_handler.execute("""UPDATE USER SET name = :name 
+                            WHERE uuid = :uuid """,
+                            {'name': name, 'uuid': user_uuid})
+    if command_handler.rowcount != 1:
+        return False
+    else:
+        return True
