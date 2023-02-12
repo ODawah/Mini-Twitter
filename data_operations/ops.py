025906bfd12ef32a4a1c4bce3b9797019022f63d
ODawah <omar.adel@factmata.com>
2023-02-12 23:45:47 +0200

    added more tests for create user operation

--- data_operations/ops.py
+++ data_operations/ops.py
@@ -1,6 +1,7 @@
 import uuid
 from database.sql_connector import connect
 from utilities.hasher import encrypt
+from utilities.validate import is_valid_email,is_valid_user_name
 
 
 # create_user: function takes user's class and add user to the database then return bool of success
@@ -8,13 +9,17 @@ def create_user(user):
     db = connect()
     if db is None:
         return None
+
+    if not is_valid_user_name(user.name) or not is_valid_email(user.email):
+        return None
     command_handler = db.cursor()
     user.uuid = str(uuid.uuid4())
     user.password = str(encrypt(user.password))
+    if user.password == "":
+        return None
     command_handler.execute("INSERT INTO USER (uuid,name, email, password) VALUES (?, ?, ?, ?)",
                             (user.uuid, user.name, user.email, user.password))
     db.commit()
     if command_handler.rowcount != 1:
         return None
-
     return user
--- data_operations/test_operations.py
+++ data_operations/test_operations.py
@@ -1,18 +1,25 @@
 import unittest
 from ops import create_user
 import models.users as user
-from utilities.validate import validate_uuid
+from utilities.validate import is_valid_uuid
 import os
 
 
 class TestOperations(unittest.TestCase):
     def test_create_user(self):
-        result = create_user(user.User(uuid="", name="test", email="test@gmail.com", password="test"))
+        result = create_user(user.User(name="test", email="test@gmail.com", password="test"))
         self.assertIsNotNone(result)
-        self.assertEqual(validate_uuid(result.uuid), True)
+        self.assertEqual(is_valid_uuid(result.uuid), True)
         self.assertEqual(result.name, "test")
         self.assertEqual(result.email, "test@gmail.com")
         self.assertNotEqual(result.password, "test")
+        result = create_user(user.User(name="", email="test@gmail.com", password="test"))
+        self.assertIsNone(result)
+        result = create_user(user.User(name="", email="", password=""))
+        self.assertIsNone(result)
+        result = create_user(user.User(name="test", email="test@gmail.com", password=""))
+        self.assertIsNone(result)
+
         os.remove("twitter.db")
 
 
