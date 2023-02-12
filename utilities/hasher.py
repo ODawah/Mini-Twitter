1f39e2a704fe73d675bf0a869b57ceb428086d0d
ODawah <omar.adel@factmata.com>
2023-02-12 23:42:59 +0200

    changed the return type of the encrypt function

--- utilities/hasher.py
+++ utilities/hasher.py
@@ -4,7 +4,7 @@ import bcrypt
 # This is the function that encrypts the passwords
 def encrypt(password_str):
     if password_str == "" or password_str is None:
-        return None
+        return ""
     # convert password to byte
     bytes = password_str.encode('utf-8')
     salt = bcrypt.gensalt()
--- utilities/test_hasher.py
+++ utilities/test_hasher.py
@@ -5,7 +5,7 @@ import hasher
 class TestHasher(unittest.TestCase):
     def test_password_hasher(self):
         self.assertNotEqual(hasher.encrypt("test_pass"), "test_pass")
-        self.assertEqual(hasher.encrypt(""), None)
+        self.assertEqual(hasher.encrypt(""), "")
 
 
 if __name__ == '__main__':
