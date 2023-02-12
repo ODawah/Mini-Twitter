2452b681f6a7c46edb5ae0c58c138963097ef2bf
ODawah <omar.adel@factmata.com>
2023-02-12 23:41:49 +0200

    added data validation functions

--- utilities/test_validate.py
+++ utilities/test_validate.py
@@ -5,17 +5,32 @@ import hasher
 
 class TestUUIDValidator(unittest.TestCase):
     def test_validate_uuid(self):
-        self.assertIs(validate.validate_uuid("c303282d-f2e6-46ca-a04a-35d3d873712d"), True)
-        self.assertIs(validate.validate_uuid("c303282d-2e6-46ca-a04a-35d3d873712d"), False)
-        self.assertIs(validate.validate_uuid(""), False)
+        self.assertIs(validate.is_valid_uuid("c303282d-f2e6-46ca-a04a-35d3d873712d"), True)
+        self.assertIs(validate.is_valid_uuid("c303282d-2e6-46ca-a04a-35d3d873712d"), False)
+        self.assertIs(validate.is_valid_uuid(""), False)
 
 
 class TestPasswordValidator(unittest.TestCase):
     def test_validate_pass(self):
         hashed = hasher.encrypt("test_password")
-        self.assertIs(validate.validate_pass("test_password", hashed), True)
-        self.assertIs(validate.validate_pass("not_the_ssame_password", hashed), False)
-        self.assertIs(validate.validate_pass("", hashed), False)
+        self.assertIs(validate.is_valid_pass("test_password", hashed), True)
+        self.assertIs(validate.is_valid_pass("not_the_ssame_password", hashed), False)
+        self.assertIs(validate.is_valid_pass("", hashed), False)
+
+
+class TestNameValidator(unittest.TestCase):
+    def test_validate_name(self):
+        long_str = "adgsakmdsakgmdfklsddmfalkdmsflkmdfklsdmffdassodfdfs"
+        self.assertIs(validate.is_valid_user_name(long_str), False)
+        self.assertIs(validate.is_valid_user_name(""), False)
+        self.assertIs(validate.is_valid_user_name("test"), True)
+
+
+class TestEmailValidator(unittest.TestCase):
+    def test_validate_name(self):
+        self.assertIs(validate.is_valid_email("test@gmail.com"), True)
+        self.assertIs(validate.is_valid_email(""), False)
+        self.assertIs(validate.is_valid_email("@sdfsd@gmil.coz"), False)
 
 
 if __name__ == '__main__':
--- utilities/validate.py
+++ utilities/validate.py
@@ -1,21 +1,37 @@
 from uuid import UUID
+import re
 import bcrypt
 
 
 # validate_uuid: function takes a uuid string and make sure that it's valid
-def validate_uuid(uuid_string):
+def is_valid_uuid(uuid_string):
     if uuid_string is None:
         return False
     try:
-        val = UUID(uuid_string,version=4)
+        val = UUID(uuid_string, version=4)
     except ValueError:
-            return False
+        return False
 
     return str(val) == uuid_string
 
-# validate_pass: function takes user enterd string and the hashed string retrieved from database and return bool of matching
-def validate_pass(pass_string,encrypted_str):
+
+# function takes user entered string and the hashed string retrieved from database and return bool of matching
+def is_valid_pass(pass_string, encrypted_str):
     pass_bytes = pass_string.encode('utf-8')
-    result = bcrypt.checkpw(pass_bytes,encrypted_str)
-    
+    result = bcrypt.checkpw(pass_bytes, encrypted_str)
+
     return result
+
+
+def is_valid_user_name(name):
+    if len(name) > 50 or name == "":
+        return False
+    else:
+        return True
+
+
+def is_valid_email(email_address):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    match = re.match(regex, email_address)
+
+    return bool(match)
